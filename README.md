f8c1c264511f6d0839c307275a2af58ca81bdf1c
YourLocalTechGuy <samuelburgess462@gmail.com>
2024-11-16 15:22:54 +0000

    install script

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - this port of StepMania contains a copy of stepmania 5.1 and has all the libs and a .sh to run it in batocera
 
 # How To install
-to install, you can enter the CLI of batocera though SSH or by plugging in a keyboard and press `Ctrl+Alt=F5`
-the default user is `root` and the password is set to `linux` by default
+- to install, you can enter the CLI of batocera though SSH or by plugging in a keyboard and press `Ctrl+Alt=F5`
+- the default user is `root` and the password is set to `linux` by default
+-then, run `curl -sSL https://github.com/YourLocalTechGuy/StepMania-for-batocera/raw/main/installer.sh | bash` to automatically install it
 
---
+++ installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the architecture is ARM64
+if [ "$(uname -m)" == "aarch64" ]; then
+    echo "Installing StepMaina..."
+    cd /userdata/roms/ports/
+    wget https://github.com/YourLocalTechGuy/StepMania-for-batocera/archive/refs/heads/main.zip -O StepMania-for-batocera.zip 
+# downloads a pre compiled version i made myself
+    unzip StepMania-for-batocera.zip
+    mv StepMania-for-batocera-main/* .
+    rm StepMaina-for-Batocera.zip
+    rm -R StepMania-for-batocera-main/
+# code above sets up the file structure for ti to work
+    echo "Installed successfully! , stepmaina should now be available in the Ports section in batocera!" 
+    rm -- "$0"
+
+else
+    echo "This version of StepMaina only works with a Raspberry pi 5 or a Raspberry Pi 4"
+    exit 1
+fi
